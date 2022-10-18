20530e58981c615525858f33044abd41244cda7e
MoonMinHyuk1 <moon981018@gmail.com>
2022-10-18 22:32:17 +0900

    :sparkles: Feat: properties
    properties

--- src/main/java/spring/study/controller/HelloController.java
+++ src/main/java/spring/study/controller/HelloController.java
@@ -1,12 +1,20 @@
 package spring.study.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.study.domain.Hello;
+import spring.study.repository.HelloRepository;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+    private final HelloRepository helloRepository;
+
     @GetMapping("/hello")
     public String hello() {
+        helloRepository.save(new Hello("hello"));
+
         return "hello";
     }
 
--- src/test/java/spring/study/StudyApplicationTests.java
+++ src/test/java/spring/study/StudyApplicationTests.java
@@ -4,22 +4,24 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import spring.study.domain.Hello;
 import spring.study.repository.HelloRepository;
 
-//@SpringBootTest
-//@Transactional
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
 class StudyApplicationTests {
-//	@Autowired
-//	private HelloRepository helloRepository;
-//
-//	@Test
-//	public void findByName() {
-//		helloRepository.save(new Hello("test"));
-//
-//		Hello hello = helloRepository.findByName("test");
-//		Assertions.assertThat(hello.getName()).isEqualTo("test");
-//	}
+	@Autowired
+	private HelloRepository helloRepository;
+
+	@Test
+	public void findByName() {
+		helloRepository.save(new Hello("test"));
+
+		Hello hello = helloRepository.findByName("test");
+		Assertions.assertThat(hello.getName()).isEqualTo("test");
+	}
 
 }
