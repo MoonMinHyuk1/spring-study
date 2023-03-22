b9ecf9be95e2af178d3e4c909d277b08ca6053f7
MoonMinHyuk1 <moon981018@gmail.com>
2023-03-22 16:18:37 +0900

    domain

---
+++ mvc/item-service/src/main/java/spring/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,41 @@
+package spring.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();   //static
+    private static long sequence = 0L;  //static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values()); //값을 add 하거나 변해도 변함이 없이 하기위해서 ArrayList로 감싼 후 반환
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
