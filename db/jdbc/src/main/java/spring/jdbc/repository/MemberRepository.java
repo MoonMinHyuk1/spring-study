d9da700107fb07830fe528fdde0b57bc2426f36e
MoonMinHyuk1 <moon981018@gmail.com>
2023-04-20 03:34:06 +0900

    sprint exception

---
+++ db/jdbc/src/main/java/spring/jdbc/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package spring.jdbc.repository;
+
+import spring.jdbc.domain.Member;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Member findById(String memberId);
+
+    void update(String memberId, int money);
+
+    void delete(String memberId);
+}
