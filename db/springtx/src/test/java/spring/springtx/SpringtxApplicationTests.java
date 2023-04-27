9a0a8eb67f301c64a983e920b2fc68713b556501
MoonMinHyuk1 <moon981018@gmail.com>
2023-04-27 17:59:26 +0900

    initial

---
+++ db/springtx/src/test/java/spring/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
