72a2e42c1a3c49229aaa066a51e961955aaea16b
MoonMinHyuk1 <moon981018@gmail.com>
2023-12-16 00:02:28 +0900

    pessimistic lock

--- stock/src/main/java/spring/stock/repository/StockRepository.java
+++ stock/src/main/java/spring/stock/repository/StockRepository.java
@@ -1,7 +1,18 @@
 package spring.stock.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spring.stock.domain.Stock;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    // Pessimistic 락은 충돌이 빈번하게 일어난다면 성능이 좋을 수 있다.
+    // 락을 통해 데이터를 제어하기 때문에 데이터 정합성이 보장된다.
+    // 락을 사용하기 때문에 성능 감소가 있을 수 있다.
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(@Param("id") Long id);
 }
---
+++ stock/src/test/java/spring/stock/service/PessimisticLockStockServiceTest.java
@@ -0,0 +1,60 @@
+package spring.stock.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import spring.stock.domain.Stock;
+import spring.stock.repository.StockRepository;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PessimisticLockStockServiceTest {
+
+    @Autowired
+    private PessimisticLockStockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.save(Stock.builder().productId(1L).quantity(100L).build());
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        //given
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        //when
+        for(int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        //then
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertThat(stock.getQuantity()).isEqualTo(0L);
+    }
+}
