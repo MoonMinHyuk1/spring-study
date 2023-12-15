62a47b0237a16be2a9e8ecfed3b4530a453f058a
MoonMinHyuk1 <moon981018@gmail.com>
2023-12-16 00:50:30 +0900

    optimistic lock

--- stock/src/main/java/spring/stock/repository/StockRepository.java
+++ stock/src/main/java/spring/stock/repository/StockRepository.java
@@ -15,4 +15,11 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select s from Stock s where s.id = :id")
     Stock findByIdWithPessimisticLock(@Param("id") Long id);
+
+    // Optimistic 락은 별도의 락을 잡지 않으므로 Pessimistic 락보다 성능상 이점이 있다.
+    // (버전) 업데이트가 실패했을 때 재시도 로직을 개발자가 작성해주어야 한다.
+    // 충돌이 빈번하게 일어나거나 그럴 것이라고 예상된다면 성능이 좋지 않다.
+    @Lock(LockModeType.OPTIMISTIC)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithOptimisticLock(@Param("id") Long id);
 }
