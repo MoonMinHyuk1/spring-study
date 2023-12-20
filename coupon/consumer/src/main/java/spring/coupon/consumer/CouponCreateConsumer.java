db272ca6b725b75cfb2714d95cfaf87c32b865ea
MoonMinHyuk1 <moon981018@gmail.com>
2023-12-21 03:55:23 +0900

    kafka consumer

--- coupon/consumer/src/main/java/spring/coupon/consumer/CouponCreateConsumer.java
+++ coupon/consumer/src/main/java/spring/coupon/consumer/CouponCreateConsumer.java
@@ -1,15 +1,22 @@
 package spring.coupon.consumer;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import spring.coupon.domain.Coupon;
+import spring.coupon.repository.CouponRepository;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class CouponCreateConsumer {
+
+    private final CouponRepository couponRepository;
 
     @KafkaListener(topics = "coupon_create", groupId = "group_1")
     public void listener(Long userId) {
         log.info("user_id : {}", userId);
+        couponRepository.save(Coupon.builder().userId(userId).build());
     }
 }
