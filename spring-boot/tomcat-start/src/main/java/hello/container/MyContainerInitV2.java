70e37b2a0c2c07adeaaa2ee8c734f728f65af424
MoonMinHyuk1 <moon981018@gmail.com>
2024-07-03 17:58:13 +0900

    servlet container init

---
+++ spring-boot/tomcat-start/src/main/java/hello/container/MyContainerInitV2.java
@@ -0,0 +1,38 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
+@HandlesTypes(AppInit.class)
+public class MyContainerInitV2 implements ServletContainerInitializer {
+
+    /**
+     * 서블릿 컨테이너 초기화
+     */
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        System.out.println("MyContainerInitV2.onStartup");
+        System.out.println("MyContainerInitV2 c = " + c);
+        System.out.println("MyContainerInitV2 ctx = " + ctx);
+
+        /**
+         * 애플리케이션 초기화
+         *
+         * HandlesTypes 어노테이션이 AppInit을 구현한 모든 class를 c에 넣어줌
+         */
+        for (Class<?> appInitClass : c) {
+            try {
+                //new AppInitV1Servlet()과 같은 코드
+                AppInit appInit = (AppInit) appInitClass.getDeclaredConstructor().newInstance();
+                appInit.onStartup(ctx);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
