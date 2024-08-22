688525f372050655d9a0cdb6cf838af3661fe2db
MoonMinHyuk1 <moon981018@gmail.com>
2024-08-22 14:59:45 +0900

    apply config

--- msa-practice/user-service/src/main/java/msa/user_service/controller/UserController.java
+++ msa-practice/user-service/src/main/java/msa/user_service/controller/UserController.java
@@ -1,2 +1,24 @@
-package msa.user_service.controller;public class UserController {
+package msa.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/user-service")
+@RestController
+public class UserController {
+
+    private final Environment env;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in User Service "
+                + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                + ", port(server.port)=" + env.getProperty("server.port")
+                + ", token secret=" + env.getProperty("token.secret")
+                + ", token expiration time=" + env.getProperty("token.expiration-time"));
+    }
 }
